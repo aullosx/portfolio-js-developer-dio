ef3b4618e70c4cff7925d773a830c2b1b542412a
aullosx <luisoliveirapk@gmail.com>
2025-08-19 20:34:02 -0300

    atualização

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,7 +5,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/aullosx/portfolio-js-developer-dio/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
